02458ef754c077e831831a9e92458dbe28285ec5
Marc Victoria <marcvictoria78@gmail.com>
2022-05-09 19:16:54 -0700

    Completed Ch 4

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
-import data from '../testData.json'
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
@@ -10,9 +10,13 @@ class App extends React.Component {
   };
   componentDidMount() {
     // timers, listeners
-    this.setState({
-      contests: data.contests
-    });
+    axios.get('/api/contests')
+      .then(resp => {
+        this.setState({
+          contests: resp.data.contests
+        });
+      })
+      .catch(console.error);
   }
   componentWillUnmount() {
     // clean timers, listeners
